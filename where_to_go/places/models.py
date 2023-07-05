5550d95af42ad4a26e8b458b4f3175b0e89a3acf
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-07-06 04:33:29 +1000

    Add model for images.

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -13,8 +13,8 @@ class Place(models.Model):
     description_long = models.TextField(
         verbose_name='Развёрнутое описание'
     )
-    сoordinates_lng = models.FloatField()
-    сoordinates_lat = models.FloatField()
+    coordinates_lng = models.FloatField()
+    coordinates_lat = models.FloatField()
 
     def __str__(self):
         return self.title
@@ -22,3 +22,24 @@ class Place(models.Model):
     class Meta:
         verbose_name = 'Место'
         verbose_name_plural = 'Места'
+
+
+class Image(models.Model):
+    place = models.ForeignKey(
+        Place,
+        on_delete=models.CASCADE,
+        verbose_name='Место',
+    )
+    image = models.ImageField(
+        verbose_name='Изображение',
+    )
+    order = models.PositiveIntegerField(
+        verbose_name='Порядковый номер',
+    )
+
+    def __str__(self):
+        return f'{self.order} изображение {self.place.title}'
+
+    class Meta:
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Изображения'
--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -120,12 +120,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-#  STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 
 # End static files
+
+# Media files
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# End media files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
