3dfe0901a5435d334c927af13574da0a3eaab988
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-07-08 02:45:06 +1000

    Add environs to project for operating with secret env variables.

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from environs import Env
+
+
+env = Env()
+env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,13 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2r84aqn8lj159ii-35x#p%(x85y)@vjtdqqrz*ianufnv=rq%7'
+SECRET_KEY = env.str('SECRET_KEY', default='ItIsSoMuchSecretKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['127.0.0.1']
-
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default='127.0.0.1')
 
 # Application definition
 
