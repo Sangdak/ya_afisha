a2c4bd08afc3fadb27076bde78ac0f1ab66ed0ce
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-07-08 00:33:13 +1000

    Add to admin inline images setup.

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -3,9 +3,13 @@ from django.contrib import admin
 from .models import Place, Image
 
 
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ['title']
+    inlines = (ImageInline,)
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
