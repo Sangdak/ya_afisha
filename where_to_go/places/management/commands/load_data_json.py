7a21eca71d514790ba147c03472c19788576a39e
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-07-09 08:13:25 +1000

    Create 'load_data_json' management command.

---
+++ where_to_go/places/management/commands/load_data_json.py
@@ -0,0 +1,48 @@
+from django.core.management.base import BaseCommand
+from django.core.files.base import ContentFile
+
+import requests
+
+from places.models import Place, Image
+
+
+def get_or_create_place(place_data):
+    place_entry, created = Place.objects.get_or_create(
+        title=place_data['title'],
+        defaults={
+            'description_short': place_data.get('description_short', ''),
+            'description_long': place_data.get('description_long', ''),
+            'coordinates_lng': place_data.get('coordinates', {}).get('lng'),
+            'coordinates_lat': place_data.get('coordinates', {}).get('lat'),
+        }
+    )
+
+    if created and place_data['imgs']:
+        # images = []
+        for order_num, image_url in enumerate(place_data.get('imgs'), start=1):
+            response = requests.get(image_url)
+            response_image = response.content
+            image_file = ContentFile(response_image)
+
+            images_entry = Image.objects.create(place=place_entry, order=order_num)
+            images_entry.image.save(f'{order_num}.jpg', image_file, save=True)
+
+
+class Command(BaseCommand):
+    help = 'Load new place data from remote *.json file.'
+
+    def handle(self, *args, **options):
+        url = options['url']
+
+        response = requests.get(url)
+        response.raise_for_status()
+        place = response.json()
+
+        get_or_create_place(place)
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'url',
+            type=str,
+            help='Link to download file.'
+        )
